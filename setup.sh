977ddb0542c80f604baa7429fc07067a1a61cf46
Tushar <gwalchmei@gmail.com>
2025-08-08 02:06:21 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
-if [ $EUID != 0 ]; then
-        echo -e "Please run script as root user\n"
-        exit 2
+
+if [ "$EUID" -ne 0 ]; then
+    echo -e "Please run script as root user\n"
+    exit 2
 fi
 
+# Variables
+USERNAME="$SUDO_USER"
+USER_HOME="/home/$USERNAME"
+
+# Update and install packages
 apt update
+apt install -y terminator fastfetch xdotool git curl
 
-cd /home/$SUDO_USER
-
-apt install terminator -y
-
+# Set Ctrl+T as shortcut for Terminator in XFCE
 xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary>t" -n -t string -s "/usr/bin/terminator"
 
-git clone https://github.com/dracula/terminator.git
+# Install Dracula theme for Terminator
+cd "$USER_HOME"
+sudo -u "$USERNAME" git clone https://github.com/dracula/terminator.git
+sudo -u "$USERNAME" bash ./terminator/install.sh
 
-./terminator/install.sh
+# Ensure terminator config directory exists
+mkdir -p "$USER_HOME/.config/terminator"
+curl -sS https://raw.githubusercontent.com/gwalchmei151/tushar-personal-configs/refs/heads/main/terminator_config \
+    -o "$USER_HOME/.config/terminator/config"
 
-curl -S https://raw.githubusercontent.com/gwalchmei151/tushar-personal-configs/refs/heads/main/terminator_config -o /home/$SUDO_USER/.config/terminator/config
+# Install Starship prompt
+sudo -u "$USERNAME" sh -c 'curl -sS https://starship.rs/install.sh | sh'
 
-curl -sS https://starship.rs/install.sh | sh
+# Set up Starship config
+mkdir -p "$USER_HOME/.config"
+curl -sS https://raw.githubusercontent.com/gwalchmei151/tushar-personal-configs/refs/heads/main/starship-pastel-w-timing-vienv.toml \
+    -o "$USER_HOME/.config/starship.toml"
 
-curl -S https://raw.githubusercontent.com/gwalchmei151/tushar-personal-configs/refs/heads/main/starship-pastel-w-timing-vienv.toml -o /home/$SUDO_USER/.config/starship.toml
+# Set up Fastfetch config
+mkdir -p "$USER_HOME/.config/fastfetch"
+curl -sS https://raw.githubusercontent.com/gwalchmei151/tushar-personal-configs/refs/heads/main/config.jsonc \
+    -o "$USER_HOME/.config/fastfetch/config.jsonc"
 
-apt install fastfetch -y
+# Replace .zshrc
+rm -f "$USER_HOME/.zshrc"
+curl -sS https://raw.githubusercontent.com/gwalchmei151/tushar-personal-configs/refs/heads/main/.zshrc \
+    -o "$USER_HOME/.zshrc"
 
-mkdir -p /home/$SUDO_USER/.config/fastfetch
+# Set correct ownership for all new files
+chown -R "$USERNAME:$USERNAME" "$USER_HOME/.config" "$USER_HOME/.zshrc" "$USER_HOME/terminator"
 
-curl -S https://raw.githubusercontent.com/gwalchmei151/tushar-personal-configs/refs/heads/main/config.jsonc -o /home/$SUDO_USER/.config/fastfetch/config.jsonc
+# Run Fastfetch once for visual confirmation (optional)
+sudo -u "$USERNAME" fastfetch -c "$USER_HOME/.config/fastfetch/config.jsonc"
 
-rm /home/$SUDO_USER/.zshrc
-
-curl -S https://raw.githubusercontent.com/gwalchmei151/tushar-personal-configs/refs/heads/main/.zshrc -o /home/$SUDO_USER/.zshrc
-
-fastfetch -c /home/$SUDO_USER/.config/fastfetch/config.jsonc
-
-xdotool key ctrl+t 
-#sudo -u $SUDO_USER zsh -c "source ~/.zshrc"
+# Simulate Ctrl+T to launch Terminator (optional, only works in GUI environment)
+sudo -u "$USERNAME" xdotool key ctrl+t
